53ea285a829895dd300f6f95b0dc76c524ca9323
fei long <614183595@qq.com>
2016-03-08 21:42:49 +0800

    update config.java

--- webapp/src/main/java/cn/saymagic/weixin/server/Config.java
+++ webapp/src/main/java/cn/saymagic/weixin/server/Config.java
@@ -3,9 +3,9 @@ package cn.saymagic.weixin.server;
 public class Config {
 
 	/** 微信公众号的token，用于验证之用*/
-	public static final String TOKEN = "";
+	public static final String TOKEN = "test";
 	
 	/** 图灵机器人的APPKEY，可在图灵官方申请*/
-	public static final String APIKEY = ""; 
+	public static final String APIKEY = "bef84f2d67fe7eb486740ce36b8681f3"; 
 	
 }
